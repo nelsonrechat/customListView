64ae62fc144960e3884aa9faefaa2fd97a8eb68f
nelsonrechat <159147225+nelsonrechat@users.noreply.github.com>
2024-03-07 09:35:29 -0500

    Creado el Adapter

---
+++ app/src/main/java/com/moviles/customlistview/ListAdapter.java
@@ -0,0 +1,51 @@
+package com.moviles.customlistview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ListAdapter extends BaseAdapter {
+    Context Contexto;
+    String[] Nombres,NumeroTelefono;
+    int[] IdImagen;
+    //El layoutInflater sirve para poner datos (inflar) en una interfaz gráfica
+    LayoutInflater inflater;
+    public ListAdapter (Context contexto,String[] nombres,String[] numeroTelefono,int[] idImagen){
+        this.Contexto = contexto;
+        this.Nombres = nombres;
+        this.NumeroTelefono = numeroTelefono;
+        this.IdImagen = idImagen;
+        inflater  = LayoutInflater.from(Contexto);
+    }
+
+    @Override
+    public int getCount() {
+        return Nombres.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        view = inflater.inflate(R.layout.list_item,null);
+        ImageView fotoPerfil = view.findViewById(R.id.fotoPerfilUsuario);
+        TextView nombreUsuario  =view.findViewById(R.id.nombre);
+        TextView telUsuario = view.findViewById(R.id.telefono);
+        fotoPerfil.setImageResource(IdImagen[i]);//poner la imagen
+        nombreUsuario.setText(Nombres[i]);
+        telUsuario.setText(NumeroTelefono[i]);
+        return view;
+    }
+}
--- app/src/main/java/com/moviles/customlistview/MainActivity.java
+++ app/src/main/java/com/moviles/customlistview/MainActivity.java
@@ -3,12 +3,32 @@ package com.moviles.customlistview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listaPersonas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int [] fotoPerfil ={
+            R.drawable.persona,
+            R.drawable.ic_launcher_foreground,
+        };
+
+        String[] nombres={
+
+        };
+
+        String[] telefonos={};
+
+
+
+        ListAdapter personas = new ListAdapter(MainActivity.this,nombres,telefonos,fotoPerfil);
+        listaPersonas=(ListView) findViewById(R.id.listaUsuarios);
+        listaPersonas.setAdapter(personas);
     }
 }
